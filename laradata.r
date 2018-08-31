2ea5c9da6123fa5e5f23166c174a6e62f696608e
Md Moudud Hasan <hasanmoudud@gmail.com>
2018-08-31 13:21:10 +0200

    new files

--- laradata.r
+++ laradata.r
@@ -1,36 +1,53 @@
 library(readr)
-Ra_226D_lara <- read_delim("D:/PhD/Simulation_data/Ra-226D.lara.txt", 
+#'''Read an lara text file and write an spectra file for PENELOPE''''
+
+####..... Input data ....#####
+nuclide = "Ra-226D" # nucliede "symbol-massnumber-m/D(If needed)"
+threshhold_intensity = 0.9   # Minimum intensity in pwersent % (0.90%)
+bin.size <- 0.0001  # Bin size of spectra   0.0001= 100 eV
+
+# Generate file name 
+lara_data_file = sprintf("%s.lara.txt", nuclide)
+output_file =  sprintf("%s_spectr.txt", nuclide)
+
+# download lara data file 
+url_lara = sprintf("http://www.nucleide.org/DDEP_WG/Nuclides/%s", lara_data_file)
+download.file(url_lara , lara_data_file)
+
+# Read text file 
+data_lara <- read_delim(lara_data_file, 
                            ";", escape_double = FALSE, trim_ws = TRUE, 
-                           skip = 13)
+                           skip = 26)
 
-Ra_226D_lara$`Energy (keV)` <-  as.numeric(Ra_226D_lara$`Energy (keV)`)
-Ra_226D_lara = Ra_226D_lara[-nrow(Ra_226D_lara), ]
+# Convert Energy from character to numeric 
+data_lara$`Energy (keV)` <-  as.numeric(data_lara$`Energy (keV)`)
 
-Ra_226 = subset(Ra_226D_lara, Ra_226D_lara$`Intensity (%)`>.9)
+#Remove last line with NA values 
+data_lara = data_lara[-nrow(data_lara), ]
 
-colnames(Ra_226)[c(1,3,5)] <- c('Energy_ev','Intensity_frac', 'Type')
+# Make a subset for a threshhold intensity (eg. > 0.9 )
+data_sub = subset(data_lara, data_lara$`Intensity (%)`>threshhold_intensity)
 
-Ra_226 = subset(Ra_226, Ra_226$Type !='a')
+# Change the column name of requried data
+colnames(data_sub)[c(1,3,5)] <- c('Energy_ev','Intensity_frac', 'Type')
 
-output <- matrix(ncol=ncol(Ra_226), nrow=nrow(Ra_226))
+# Remove alfa lines 
+data_sub = subset(data_sub, data_sub$Type !='a')
 
-for(i in 1:(nrow(Ra_226)*2)){
-  output[i,] <- runif(2)
-  cat(sprintf("<set name=\"%s\" value=\"%f\" ></set>\n", df$timeStamp, df$Price))
-  file.create("sample.txt")
-  fileConn <- file("sample.txt")
-  writeLines(c(paste(i, j, k, "07"),"1","41.6318 -87.0881   10.0"), fileConn)
-  close(fileConn)
-  file.show("sample.txt")
-  
+# Create and open an text file to write the spectra 
+file.create(output_file)
+fileConn1 <- file(output_file, "w")
+
+#Write the spectra in text file .... two lines for a bin..... intensity is -1 for last line 
+for(i in 1:nrow(data_sub)){
+  write(sprintf("SPECTR   \ %fe6\    \ %f\ ", (data_sub$Energy_ev[i]*0.001), (data_sub$Intensity_frac[i]/100)), fileConn1, append=TRUE)
+  if(i != nrow(data_sub)){
+    write(sprintf("SPECTR   \ %fe6\    \ %f\ ", ((data_sub$Energy_ev[i]*0.001)+bin.size), (0)), fileConn1, append=TRUE)
+  }else{
+    write(sprintf("SPECTR   \ %fe6\    \ %f\ ", ((data_sub$Energy_ev[i]*0.001)+bin.size), (-1)), fileConn1, append=TRUE)
+  }
 }
+close(fileConn1)
+file.show("sample1.txt")
 
-for(i in seq_along(lst)) {
-  write.table(lst[[i]], paste(names(lst)[i], ".txt", sep = ""), 
-              col.names = FALSE, row.names = FALSE, sep = "\t", quote = FALSE)
-}
 
-output
-
-output <- data.frame(output)
-class(output)
