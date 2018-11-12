40df5404868893c716e88bd5f423835184b3c329
Md Moudud Hasan <hasanmoudud@gmail.com>
2018-11-12 09:55:29 +0100

    up

--- laradata_nodownload_allnuclides.r
+++ laradata_nodownload_allnuclides.r
@@ -1,12 +1,14 @@
+rm(list = ls())
+
 library(utils)
 #'''Read an lara text file and write an spectra file for PENELOPE''''
 
 ####..... Input data ....#####
-nuclide1 = "Ba-133" # nucliede "symbol-massnumber-m/D(If needed)"
-nuclide2 = "Co-60" # nucliede "symbol-massnumber-m/D(If needed)"
+nuclide1 = "Am-241" # nucliede "symbol-massnumber-m/D(If needed)"
+nuclide2 = "Cs-137" # nucliede "symbol-massnumber-m/D(If needed)"
 nuclide3 = "Eu-152" # nucliede "symbol-massnumber-m/D(If needed)"
 nuclide4 = "Eu-154" # nucliede "symbol-massnumber-m/D(If needed)"
-threshhold_intensity = 0.49   # Minimum intensity in pwersent % (0.90%)
+threshhold_intensity = 0.01  # Minimum intensity in pwersent % (0.90%)
 bin.size <- 0.0001  # Bin size of spectra   0.0001= 100 eV
 
 # Generate file name 
@@ -15,16 +17,19 @@ output_file =  sprintf("All_nuclides_%s_spectr_thrsh_%f_inp.txt", nuclide1, thre
 lara_data_read <- function(nuclide,threshhold_intensity){
   # Read lara data file 
   lara_data_file = sprintf("%s.lara.txt", nuclide)
-  url_lara = sprintf("http://www.nucleide.org/DDEP_WG/Nuclides/%s", lara_data_file)
+  # Read lara data file 
+  url_lara = sprintf("http://www.lnhb.fr/nuclides/%s", lara_data_file)
   all_content = readLines(url_lara)
   Head_skip = all_content[-c(1:grep("--------",all_content))]
   data_lara <- read.table(textConnection(Head_skip[-length(Head_skip)]), header = TRUE, sep = ";", quote = "")
-  
+
   # Make a subset for a threshhold intensity (eg. > 0.9 )
-  data_sub = subset(data_lara, data_lara$Intensity.... > threshhold_intensity)
-  
+  data_sub = data_lara[which(data_lara$Intensity.... > threshhold_intensity), ]
+
   # Change the column name of requried data
   colnames(data_sub)[c(1,3,5)] <- c('Energy_ev','Intensity_frac', 'Type')
+  data_sub = data_sub[order(data_sub$Energy_ev), ]
+  data_sub = data_sub[ which(data_sub$Type != ' a '), ]
   data_sub_only = data_sub[c(1,3,5)]
   return(data_sub_only)
 }
@@ -41,23 +46,30 @@ data_all = merge(x = data_all, y = nuclide3_data, by = c('Energy_ev','Intensity_
 data_all = merge(x = data_all, y = nuclide4_data, by = c('Energy_ev','Intensity_frac', 'Type'), all = TRUE)
 
 # Remove alfa lines 
-data_all = subset(data_all, data_all$Type !='a')
-
+data_all = subset(data_all, data_all$Type != 'a')
+data_all = data_all[order(data_all$Energy_ev), ]
 # Create and open an text file to write the spectra 
 file.create(output_file)
 fileConn1 <- file(output_file, "w")
 
 #Write the spectra in text file .... two lines for a bin..... intensity is -1 for last line 
 for(i in 1:nrow(data_all)){
-  #write(sprintf('myfile.write ("SPECTR   \ %fe6\   \ %f\\n")', (data_all$Energy_ev[i]*0.001), (data_all$Intensity_frac[i]/100)), fileConn1, append=TRUE)
+  if (i != nrow(data_all)){
+  bin_ck = (data_all$Energy_ev[i+1]*0.001)-(data_all$Energy_ev[i]*0.001)
+  if (bin_ck < bin.size){
+    bin_add = ((data_all$Energy_ev[i+1]*0.001)-(data_all$Energy_ev[i]*0.001))/2.0
+  }else{
+    bin_add = bin.size
+  }
+  }
   write(sprintf("SPECTR   \ %fe6\   \ %f", (data_all$Energy_ev[i]*0.001), (data_all$Intensity_frac[i]/100)), fileConn1, append=TRUE)
   if(i != nrow(data_all)){
-    #write(sprintf('myfile.write ("SPECTR   \ %fe6\   \ %f\\n")', ((data_all$Energy_ev[i]*0.001)+bin.size), (0)), fileConn1, append=TRUE)
-    write(sprintf("SPECTR   \ %fe6\   \ %f", ((data_all$Energy_ev[i]*0.001)+bin.size), (0)), fileConn1, append=TRUE)
+    write(sprintf("SPECTR   \ %fe6\   \ %f", ((data_all$Energy_ev[i]*0.001)+bin_add), (0)), fileConn1, append=TRUE)
   }else{
-    #write(sprintf('myfile.write ("SPECTR   \ %fe6\   \ %f\\n")', ((data_all$Energy_ev[i]*0.001)+bin.size), (-1)), fileConn1, append=TRUE)
-    write(sprintf("SPECTR   \ %fe6\   \ %f", ((data_all$Energy_ev[i]*0.001)+bin.size), (-1)), fileConn1, append=TRUE)
+    write(sprintf("SPECTR   \ %fe6\   \ %f", ((data_all$Energy_ev[i]*0.001)+bin_add), (-1)), fileConn1, append=TRUE)
   }
 }
+write(sprintf("C   Total eimission probability  \ %f", (sum(data_all$Intensity_frac)/100)), fileConn1, append=TRUE)
 close(fileConn1)
 file.show(output_file)
+plot(data_all$Energy_ev, data_all$Intensity_frac, "l")
--- laradata_nodownload_penelope_TLD_CF_EM.r
+++ laradata_nodownload_penelope_TLD_CF_EM.r
@@ -8,7 +8,7 @@ bin.size <- 0.0001  # Bin size of spectra   0.0001= 100 eV
 
 # Generate file name 
 lara_data_file = sprintf("%s.lara.txt", nuclide)
-output_file =  sprintf("%s_spectr_thrsh_%f_inp.txt", nuclide, threshhold_intensity)
+output_file =  sprintf("%s_spectr_thrsh_%f_inp_CF_EM.txt", nuclide, threshhold_intensity)
 
 # Read lara data file 
 url_lara = sprintf("http://www.lnhb.fr/nuclides/%s", lara_data_file)
@@ -26,7 +26,8 @@ data_sub = data_sub[ which(data_sub$Type != ' a '), ]
 # Remove alfa lines 
 data_sub = data_sub[ ,c(1,3,5)] 
 
-
+data_sub$Conversion = (-3*10^(-10) * data_sub$Energy^3) + (1*10^(-6) * data_sub$Energy^2) - (0.0011*data_sub$Energy) + 1.3105
+data_sub$CfEm = data_sub$Conversion * data_sub$Intensity_frac
 # Create and open an text file to write the spectra 
 file.create(output_file)
 fileConn1 <- file(output_file, "w")
@@ -41,17 +42,17 @@ for(i in 1:nrow(data_sub)){
     bin_add = bin.size
   }
   }
-  write(sprintf("SPECTR   \ %fe6\   \ %f", (data_sub$Energy_ev[i]*0.001), (data_sub$Intensity_frac[i]/100)), fileConn1, append=TRUE)
+  write(sprintf("SPECTR   \ %fe6\   \ %f", (data_sub$Energy_ev[i]*0.001), (data_sub$CfEm[i]/100)), fileConn1, append=TRUE)
   if(i != nrow(data_sub)){
     write(sprintf("SPECTR   \ %fe6\   \ %f", ((data_sub$Energy_ev[i]*0.001)+bin_add), (0)), fileConn1, append=TRUE)
   }else{
     write(sprintf("SPECTR   \ %fe6\   \ %f", ((data_sub$Energy_ev[i]*0.001)+bin_add), (-1)), fileConn1, append=TRUE)
   }
 }
-write(sprintf("C   Total eimission probability  \ %f", (sum(data_sub$Intensity_frac)/100)), fileConn1, append=TRUE)
+write(sprintf("C   Total eimission probability  \ %f", (sum(data_sub$CfEm)/100)), fileConn1, append=TRUE)
 close(fileConn1)
 file.show(output_file)
-plot(data_sub$Energy_ev, data_sub$Intensity_frac, "p")
+plot(data_sub$Energy_ev, data_sub$CfEm, "p")
 
 
 
